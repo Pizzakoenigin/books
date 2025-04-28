a65bf61f1d834e90ddcbda15618cae80328b3847
Franziska <Franziska.Roder@gmx.de>
2025-04-28 16:43:41 +0200

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+const book1 = new Book('It', 'Stephen King', 300, true)
+
+console.log(book1.info());
